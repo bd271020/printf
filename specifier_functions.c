06cb25a1e4f978a98ee8d1a04102f040743a60f7
Samuel Eleshinnla <samelesh1@gmail.com>
2023-03-28 20:19:21 -0700

    Updated with print integer

--- specifier_functions.c
+++ specifier_functions.c
@@ -47,8 +47,27 @@ int print_percent(__attribute__((unused))va_list args)
  */
 int print_integer(va_list args)
 {
-	int num;
+	int n, d, i;
+	unsigned int x;
 
-	num = print_number(args);
-	return (num);
+	n = va_arg(args, int);
+	d = 1;
+	i = 0;
+
+	if (n < 0)
+	{
+		i = _putchar('-');
+		x = n * (-1);
+	}
+	else
+		x = n;
+	while ((x / d) > 10)
+		d = d * 10;
+	while (d != 0)
+	{
+		i = i + _putchar('0' + (x / d));
+		x = x % d;
+		d = d / 10;
+	}
+	return (i);
 }
