60c52fb2264a1883e069b831e7e81ec084b3b809
Samuel Eleshinnla <samelesh1@gmail.com>
2023-03-31 08:31:41 -0700

    Solving for %d and %i

--- specifier_functions.c
+++ specifier_functions.c
@@ -47,27 +47,8 @@ int print_percent(__attribute__((unused))va_list args)
  */
 int print_integer(va_list args)
 {
-	int n, d, i;
-	unsigned int x;
+	int num_length;
 
-	n = va_arg(args, int);
-	d = 1;
-	i = 0;
-
-	if (n < 0)
-	{
-		i = _putchar('-');
-		x = n * (-1);
-	}
-	else
-		x = n;
-	while ((x / d) > 10)
-		d = d * 10;
-	while (d != 0)
-	{
-		i = i + _putchar('0' + (x / d));
-		x = x % d;
-		d = d / 10;
-	}
-	return (i);
+	num_length = print_number(args);
+	return (num_length);
 }
